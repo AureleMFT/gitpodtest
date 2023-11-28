c5ba8c3b3775efe391a8b42bd13911d54c329ab0
AureleMFT MFT <laurilerichpay1@gmail.com>
2023-11-28 21:47:05 +0000

    add hello word module

---
+++ project/web/modules/custom/hello_word/src/Controller/HelloWordController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\hello_word\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for Hello word routes.
+ */
+class HelloWordController extends ControllerBase {
+
+  /**
+   * Builds the response.
+   */
+  public function build($name = Null) {
+    $out = 'It works'.($name?$name:' wh').' !';
+
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t($out),
+    ];
+
+    return $build;
+  }
+
+}
